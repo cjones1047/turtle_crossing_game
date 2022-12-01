bfd4b0ddc2b99ff480718d38fe3cfa7af13f5da3
cjones1047 <cjmj1047@gmail.com>
2022-12-01 09:50:11 -0600

    Turtle is correctly rendered upon program running via instantiation of Player class in player module, turtle moves forward upon up keypress

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+turtle = Player()
+
+screen.listen()
+screen.onkey(fun=turtle.move, key="Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
--- player.py
+++ player.py
@@ -1,7 +1,20 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
+STARTING_DIRECTION = 90
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("black")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(STARTING_DIRECTION)
+
+    def move(self):
+        self.forward(MOVE_DISTANCE)
